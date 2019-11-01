57063c2d4b2c0b27f9ba0405698cfe93521a01dd
Ayman Boustati <ayman.boustati@outlook.com>
2019-11-01 18:31:51 +0000

    built skeleton code for data generator and sampler

---
+++ robust-smc/data.py
@@ -0,0 +1,85 @@
+import numpy as np
+
+
+def peaks(x, y):
+    """
+    Re-implementation of MATLAB's peaks function
+    :param x: Nx1 Numpy array
+    :param y: Nx1 Numpy array
+    :return: Nx1 Numpy array
+    """
+    first_term = 3 * ((1 - x) ** 2) * np.exp(- (x ** 2) - ((y + 1) ** 2))
+    second_term = - 10 * ((x / 5) - (x ** 3) - (y ** 5)) * np.exp(-(x ** 2 + y ** 2))
+    third_term = - (1 / 3) * np.exp(-(x + 1) ** 2 + y ** 2)
+    return 200 * (first_term + second_term + third_term)
+
+
+def dem(x, y):
+    """
+    Synthetic Digital Elevation Model (DEM) map
+    :param x: x coordinates Nx1 numpy array
+    :param y: y coordinates Nx1 numpy array
+    :return: z cooredinates Nx1 numpy array
+    """
+    a = np.array([300, 80, 60, 40, 20, 10])[None, :]  # 1x6
+    omega = np.array([5, 10, 20, 30, 80, 150])[None, :]  # 1x6
+    omega_bar = np.array([4, 10, 20, 40, 90, 150])[None, :]  # 1x6
+    q = 3 / (2.96 * 1e4)
+    peak = peaks(q * x, q * y)
+    fourier = np.sum(a * np.sin(omega * q * x) * np.cos(omega_bar * q * x), axis=1)[:, None]
+    return peak + fourier
+
+
+class TANSimulator:
+    """
+    Terain Aided Navigation (TAN) simulator. Taken from Merlinge et. al. 2019
+    """
+    def __init__(self, final_time, time_step=0.1, observation_std=5,
+                 X0=None, transition_matrix=None, process_std=None, seed=None):
+        """
+        :param final_time: final time period
+        :param time_step: time step
+        :param observation_std: observation noise standard deviation
+        :param X0: initial state
+        :param transition_matrix: transition matrix
+        :param process_std: process standard deviation
+        :param seed: random seed
+        """
+        self.final_time = final_time
+        self.time_step = time_step
+        self.X0 = X0 or np.array([-3.0 * 1e3, -19.2 * 1e3, 1.1 * 1e3, 211.5, 215.3, 0.0])
+        self.transition_matrix = transition_matrix or np.vstack(
+            [np.hstack([np.eye(3), time_step * np.eye(3)]), np.hstack([0 * np.eye(3), np.eye(3)])]
+        )
+        self.simulation_steps = int(final_time / time_step)
+        self.observation_std = observation_std
+        self.process_std = process_std or np.array([0.1, 0.1, 0.3, 1.45 * 1e-2, 2.28 * 1e-2, 11.5 * 1e-2])
+        self.seed = seed
+        self.rng = np.random.RandomState(self.seed)
+        self._simulate_system()
+
+    @staticmethod
+    def observation_model(X):
+        """
+        TANSimulator observation model
+
+        m = z - DEM(x, y)
+
+        :param X: xy-coordinates Nx2 numpy array
+        :return: z-coordinates
+        """
+        return X[:, 3][:, None] - dem(X[:, 0][:, None], X[:, 1][:, None])
+
+    def _simulate_system(self):
+        """
+        Simulates the TAN system
+        """
+        X = np.zeros((self.simulation_steps + 1, 6))
+        X[0, :] = self.X0
+        for t in range(self.simulation_steps):
+            state_evolution = np.matmul(self.transition_matrix, X[t, :][:, None])
+            process_noise = self.process_std[:, None] * self.rng.randn(6, 1)
+            X[t + 1, :] = (state_evolution + process_noise)[:, 0]
+
+        Y = self.observation_model(X) + self.observation_std * self.rng.randn(self.simulation_steps + 1, 1)
+        self.X, self.Y = X, Y
---
+++ robust-smc/sampler.py
@@ -0,0 +1,102 @@
+from abc import ABC, abstractmethod
+
+import numpy as np
+from scipy.special import logsumexp
+from scipy.stats import norm
+
+
+class SMCSampler(ABC):
+    def __init__(self, data, num_samples=100, x_init=None, seed=None):
+        self.data = data
+        self.time_steps = self.data.shape[0]
+        self.num_samples = num_samples
+        self.x_init = x_init
+
+        self.x_samples = []
+        self.x_trajectories = []
+        self.logw = []
+        self.initialized = False
+        self.x_samples.append(self.proposal_sample(self.x_init))
+        self.logw = self.compute_logw(0)
+        self.seed = seed
+        self.rng = np.random.RandomState(seed)
+
+    def multinomial_resampling(self, w, x):
+        """
+        Standard multinomial resampling for SMCSampler
+        :param w: weights Nx1 numpy array
+        :param x: sample trajectories NxD numpy array
+        :return: NxD numpy array
+        """
+        u = self.rng.rand(*w.shape)
+        bins = np.cumsum(w)
+        return x[np.digitize(u, bins).squeeze()]
+
+    @abstractmethod
+    def proposal_sample(self, x_prev):
+        """
+        Samples from the proposal distribution
+        :param x_prev: conditioning set NxD numpy array
+        :return: NxD numpy array
+        """
+        if not self.initialized:
+            # return prior on on x_init
+            self.initialized = True
+            pass
+        else:
+            # return prior from proposal
+            pass
+
+    @abstractmethod
+    def compute_logw(self, t):
+        """
+        logarithms of importance weights
+        :return: Nx1 numpy array
+        """
+        pass
+
+    def sample(self):
+        for t in range(self.time_steps):
+            # Resample
+            w = self.get_normalised_weights(self.logw[-1])
+            self.x_trajectories.append(self.multinomial_resampling(w, self.x_samples[-1]))
+            # Sample
+            self.x_samples.append(self.proposal_sample(self.x_trajectories[-1]))
+            # Weight
+            self.logw.append(self.compute_logw(t + 1))
+
+    @staticmethod
+    def get_normalised_weights(logw):
+        """
+        Compute normalised weights from log-weights
+        :param logw: log-weights Nx1 numpy array
+        :return: Nx1 numpy array
+        """
+        return np.exp(logw - logsumexp(logw))
+
+
+class LinearDiagonalGaussianBPF(SMCSampler):
+    def __init__(self, time_steps, transition_matrix, prior_std, x_init, observation_model, num_samples=100, seed=None):
+        super().__init__(time_steps, num_samples=num_samples, x_init=x_init, seed=seed)
+        self.transition_matrix = transition_matrix
+        self.prior_std = prior_std
+        self.observation_model = observation_model
+
+    def proposal_sample(self, x_prev):
+        """
+        Samples from the proposal distribution
+        :param x_prev: conditioning set NxD numpy array
+        :return: NxD numpy array
+        """
+        self.initialized = True
+        D = x_prev.shape[1]
+        noiseless = np.matmul(self.transition_matrix, x_prev[:, :, None])[:, :, 0]
+        noise = self.prior_std[None, :] * self.rng.randn(self.num_samples, D)
+        samples = noiseless + noise
+        return samples
+
+    def compute_logw(self, t):
+        observed = np.tile(self.data[t], self.num_samples)[:, None]
+        predicted = self.observation_model(self.x_samples[-1])
+        norm.logpdf(observed, predicted)
+        return
